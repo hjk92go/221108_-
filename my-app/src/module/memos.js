32001cab1e0c2ffa72e99a7d68acffa76bdd8be6
hjk92go <hjk92go@gmail.com>
2022-11-08 16:02:37 +0900

    message

---
+++ my-app/src/module/memos.js
@@ -0,0 +1,27 @@
+
+
+const initalState = {
+    memoList : [
+        {
+            date : new Date(),
+            name : "green", 
+            text : "방명록을 작성했습니다."
+        }
+    ]
+}    
+
+
+export const addmemo = (memo) => ({type:"addmemo", payload : memo})
+
+
+const memos = (state=initalState, action) => {
+    switch (action.type) {
+        case 'addmemo': 
+            return { ...state, memoList : state.memoList.concat(action.payload) };
+        default :
+            return state;
+    }
+}
+
+export default memos;
+
